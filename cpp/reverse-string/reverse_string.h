c595a21dd7ba8539710e370829b19c9114cdb14b
Jean-Charles <jc@webelop.net>
2020-03-21 21:37:58 +0000

    Cpp: reverse-string by reverse iterator

--- cpp/reverse-string/reverse_string.h
+++ cpp/reverse-string/reverse_string.h
@@ -1,8 +1,7 @@
-#if !defined(REVERSE_STRING_H)
-#define REVERSE_STRING_H
+#include <string>
 
 namespace reverse_string {
-
+    std::string reverse_string(std::string text) {
+        return std::string(text.rbegin(), text.rend());
+    }
 }  // namespace reverse_string
-
-#endif // REVERSE_STRING_H
