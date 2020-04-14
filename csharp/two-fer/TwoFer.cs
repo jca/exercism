175ee4f0bb67fbe8aceb7a125f25cd6aa331ee71
Jean-Charles <jc@webelop.net>
2020-04-14 18:31:00 +0100

    CSharp twofer, gigasecond

---
+++ csharp/two-fer/TwoFer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public static class TwoFer
+{
+    public static string Speak(String name = null) =>
+        $"One for {Default(name)}, one for me.";
+
+    private static string Default(String name = null) =>
+        string.IsNullOrEmpty(name) ? "you" : name;
+
+}
