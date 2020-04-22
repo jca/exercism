c6a97cae2d19d0759016db232c88634dce41a024
Jean-Charles <jc@webelop.net>
2020-04-22 10:21:26 +0100

    Csharp resistor colors

---
+++ csharp/resistor-color/ResistorColor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class ResistorColor
+{
+    private static readonly Dictionary<string, int> _colors = new Dictionary<string, int>
+    {
+        {"black", 0},
+        {"brown", 1},
+        {"red", 2},
+        {"orange", 3},
+        {"yellow", 4},
+        {"green", 5},
+        {"blue", 6},
+        {"violet", 7},
+        {"grey", 8},
+        {"white", 9},
+    };
+
+    public static int ColorCode(string color)
+    {
+        return _colors[color];
+    }
+
+    public static string[] Colors()
+    {
+        return _colors.Select(e => e.Key).ToArray();
+    }
+}
