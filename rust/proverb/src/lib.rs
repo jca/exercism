419e4a66579947c66983118126372ef11436b2de
Jean-Charles Callu <jc.callu@gmail.com>
2020-10-03 23:40:49 +0100

    rust/proverbs, using windows

--- rust/proverb/src/lib.rs
+++ rust/proverb/src/lib.rs
@@ -1,16 +1,13 @@
 pub fn build_proverb(list: &[&str]) -> String {
-    let mut c: String = String::from("");
-    let mut maybe_previous: Option<&str> = None;
-    for text in list.into_iter() {
-        if let Some(previous) = maybe_previous {
-            c = format!("{}For want of a {} the {} was lost.\n", c, previous, text);
-        }
-        maybe_previous = Some(text)
+    if list.is_empty() {
+        return String::new();
     }
 
-    if let Some(first) = list.first() {
-        c = format!("{}And all for the want of a {}.", c, first);
-    }
-
-    return c;
+    list.windows(2)
+        .map(|pair| format!("For want of a {} the {} was lost.\n", pair[0], pair[1]))
+        .chain(std::iter::once(format!(
+            "And all for the want of a {}.",
+            list[0]
+        )))
+        .collect()
 }
