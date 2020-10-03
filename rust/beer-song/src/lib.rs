74a9debae31aa028e6a774445ba740e6f1e68566
Jean-Charles Callu <jc.callu@gmail.com>
2020-10-04 00:11:06 +0100

    Rust: beer song

---
+++ rust/beer-song/src/lib.rs
@@ -0,0 +1,15 @@
+pub fn verse(n: u32) -> String {
+    match n {
+        0 => String::from("No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"),
+        1 => format!("{} bottle of beer on the wall, {} bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n", n, n),
+        _ => format!("{} bottles of beer on the wall, {} bottles of beer.\nTake one down and pass it around, {} bottle{} of beer on the wall.\n", n, n, n-1, match n {2 => "", _ => "s"}),
+    }
+}
+
+pub fn sing(start: u32, end: u32) -> String {
+    (end..start + 1)
+        .rev()
+        .map(verse)
+        .collect::<Vec<String>>()
+        .join("\n")
+}
