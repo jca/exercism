6c4f660db84aa1b8f84989dce1108dfe05952865
Jean-Charles Callu <jc.callu@gmail.com>
2020-10-03 13:03:08 +0100

    Rust - Proverbs, take 1

--- rust/proverb/src/lib.rs
+++ rust/proverb/src/lib.rs
@@ -1,5 +1,16 @@
 pub fn build_proverb(list: &[&str]) -> String {
-    let mut c: String;
-    c = &"test";
+    let mut c: String = String::from("");
+    let mut maybe_previous: Option<&str> = None;
+    for text in list.into_iter() {
+        if let Some(previous) = maybe_previous {
+            c = format!("{}For want of a {} the {} was lost.\n", c, previous, text);
+        }
+        maybe_previous = Some(text)
+    }
+
+    if let Some(first) = list.first() {
+        c = format!("{}And all for the want of a {}.", c, first);
+    }
+
     return c;
 }
