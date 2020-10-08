75ff7613fdf88e1005f3beb2d2258460dac51b78
Jean-Charles Callu <jc.callu@gmail.com>
2020-10-08 22:38:23 +0100

    Rust: sum of multiples II

--- rust/sum-of-multiples/src/lib.rs
+++ rust/sum-of-multiples/src/lib.rs
@@ -1,8 +1,5 @@
 pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
-    (1..limit).fold(0u32, |acc, i| {
-        match factors.iter().any(|f| (*f != 0) && (i % f == 0)) {
-            true => acc + i,
-            false => acc,
-        }
-    })
+    (1..limit)
+        .filter(|i| factors.iter().any(|&f| f != 0 && i % f == 0))
+        .sum()
 }
